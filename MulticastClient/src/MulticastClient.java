a6af15a42c71c999fbd9ea50afa3f8df0a36069f
Joao Jose Marques Lopes <joaofcp98@hotmail.com>
2018-10-12 00:00:57 +0100

    primeiro

--- MulticastClient/src/MulticastClient.java
+++ MulticastClient/src/MulticastClient.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
  * Usage: java -Djava.net.preferIPv4Stack=true MulticastClient
  *
  * @author Raul Barbosa
- * ola fds
+ * ola
  * @version 1.0
  */
 public class MulticastClient extends Thread {
